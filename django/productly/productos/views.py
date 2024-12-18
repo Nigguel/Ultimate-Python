9d9cc939ae116b293eb4493fb632d15b775948f8
nigguel <nigguel14@gmail.com>
2024-12-17 21:03:55 -0300

    Trabajando con plantillas, freamworks de CSS, compartiendo plantillas, parametros de URL y obteniendo elementos de la base de datoos

--- django/productly/productos/views.py
+++ django/productly/productos/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+"""     views.py     """
+
+# from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Producto
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    productos = Producto.objects.all().values()  # pylint: disable=E1101
-    # print(productos)
-    return JsonResponse(list(productos), safe=False)
+    """
+    index
+    """
+    productos = Producto.objects.all()  # pylint: disable=E1101
+
+    return render(request, "index.html", context={"productos": productos})
+
+
+def detalle(request, producto_id):
+    """
+    detalle
+    """
+    producto = Producto.objects.get(id=producto_id)  # pylint: disable=E1101
+
+    return render(request, "detalle.html", context={"producto": producto})
